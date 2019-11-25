7556ceadff77b8218061227a508c030574a6dfde
Anton Kibekin <kibekin1984anton@gmail.com>
2019-11-25 15:42:58 +0300

    Простые числа. [#156314]

---
+++ Chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 1; i * i <= finish; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                if ((i % j) == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+}
