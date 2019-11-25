0ddcfc9b1706c02302fcbd77ceee9804d1404d03
Anton Kibekin <kibekin1984anton@gmail.com>
2019-11-25 15:06:28 +0300

    Простое число. [#171694]

--- Chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ Chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -2,16 +2,13 @@ package ru.job4j.loop;
 
 public class Counter {
     public int add(int start, int finish) {
-        int j = 0;
         int sum = 0;
         for (int i = 0; i <= 10; i++) {
             if (i % 2 == 0) {
-                j++;
                 sum += i;
             }
         }
         return sum;
     }
 
-//
 }
