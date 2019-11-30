1252267fa467754aa8aa48c9ec4f18765d61e064
Anton Kibekin <kibekin1984anton@gmail.com>
2019-11-30 12:31:11 +0300

    Классический поиск перебором. [#33489]

---
+++ Chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+
+public class Square {
+
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i < bound; i++) {
+            rst[i] = (int) Math.pow(i, 2);
+//            System.out.println(rst[i]);
+        }
+        return rst;
+    }
+
+
+}
