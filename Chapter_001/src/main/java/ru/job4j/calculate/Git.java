30220eb0f146464cf176472a16c7358637a0d0a6
Anton Kibekin <kibekin1984anton@gmail.com>
2019-12-09 17:21:01 +0300

    Двухмерный массив. Таблица умножения. [#33491]

--- Chapter_001/src/main/java/ru/job4j/calculate/Git.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.calculate;
-
-public class Git {
-}
--- Chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ Chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,19 +1,16 @@
-package ru.job4j.loop;
-
-public class Mortgage {
-     public int year(int amount, int salary, double percent) {
-         int year = 0;
-
-         while (amount >= 0) {
-             double с = (amount - percent) + salary;
-             double p = с - salary;
-
-             year++;
-         }
-         return year;
-     }
-
-    public static void main(String[] args) {
-        System.out.println(new Mortgage().year(100, 120, 50));
-    }
-}
+//package ru.job4j.loop;
+//
+//public class Mortgage {
+//    public int year(int amount, int salary, double percent) {
+//        int year = 0;
+//
+//        while (amount >= 0) {
+//            double с = (amount - percent) + salary;
+//            double p = с - salary;
+//
+//            year++;
+//        }
+//        return year;
+//    }
+//
+//}
